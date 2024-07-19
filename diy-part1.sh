1c13aa9c9ae20203e8e280bb79fd0fb9d936a97d
dann2333 <146356127+dann2333@users.noreply.github.com>
2024-07-19 23:51:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,18 +11,6 @@
 # fw876/helloworld
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-
-#Openclash
-echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
- 
-#Passwall
-echo 'src-git PassWall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
- 
 #HelloWorld
 echo 'src-git HelloWorld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-# Lienol/openwrt-package
-#sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
-
-echo 'src-git freifunk https://github.com/freifunk/openwrt-packages.git' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
